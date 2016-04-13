9756f8c40b216e87b4fd945e4f210f7e98751c24
Sam <ssepa001@ucr.edu>
2016-04-13 12:57:32 -0700

    Login UI setup, signup page route setup.

---
+++ client/nav.js
@@ -0,0 +1,19 @@
+
+
+Template.nav.helpers({
+  create: function(){
+
+  },
+  rendered: function(){
+
+  },
+  destroyed: function(){
+
+  },
+});
+
+Template.nav.events({
+  "click #foo": function(event, template){
+
+  }
+});
